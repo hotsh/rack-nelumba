0f35f5bd21623d3869abc43d29bd8acdc67cc9dc
wilkie <wilkie05@gmail.com>
2013-04-02 00:54:36 -0400

    Require helpers when root is required.

--- lib/rack/lotus.rb
+++ lib/rack/lotus.rb
@@ -16,4 +16,5 @@ end
 
 Dir[File.join(File.dirname(__FILE__), "lotus", "*.rb")].each {|file| require file }
 Dir[File.join(File.dirname(__FILE__), "lotus", "controllers", "*.rb")].each {|file| require file }
+Dir[File.join(File.dirname(__FILE__), "lotus", "helpers", "*.rb")].each {|file| require file }
 Dir[File.join(File.dirname(__FILE__), "lotus", "models", "*.rb")].each {|file| require file }
