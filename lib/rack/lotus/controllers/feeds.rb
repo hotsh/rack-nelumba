dffd07348355f37abd37f8aa723d6ac08d80cdba
wilkie <wilkie05@gmail.com>
2013-04-03 21:31:59 -0400

    Check access permissions for activity posting.

--- lib/rack/lotus/controllers/feeds.rb
+++ lib/rack/lotus/controllers/feeds.rb
@@ -8,8 +8,14 @@ module Rack
 
     # Add an activity to the given feed if you own it.
     post '/feeds/:id' do
-      feed = Feed.find_by_id(params[:id])
-      status 404 and return if feed.nil?
+      p = Person.find_by_id(session[:person_id])
+      if p.nil? || p.activities.feed.id != params[:id]
+        status 404
+        return
+      end
+
+      puts "Place activity"
+      puts params["content"]
     end
   end
 end
