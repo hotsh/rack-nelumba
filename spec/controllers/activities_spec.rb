d3b414096cd0629de74abb88dc54675081a4c078
wilkie <wilkie05@gmail.com>
2013-04-09 20:19:48 -0400

    Adds Activities controller tests.
    The activities controllers doesn't really do anything at the moment, so
    this is really just boiler plate.

---
+++ spec/controllers/activities_spec.rb
@@ -0,0 +1,46 @@
+require_relative 'helper'
+require_controller 'activities'
+
+class  Activity; end
+module Lotus;  end
+
+describe Rack::Lotus do
+  before do
+    # Do not render
+    Rack::Lotus.any_instance.stubs(:render).returns("html")
+  end
+
+  describe "Activities Controller" do
+    describe "GET /activities/:id" do
+      it "should return 404 if the activity is not found" do
+        Activity.stubs(:find_by_id).returns(nil)
+
+        get '/activities/1234abcd'
+        last_response.status.must_equal 404
+      end
+
+      it "should return 200 if the person is found" do
+        Activity.stubs(:find_by_id).returns("something")
+
+        get '/activities/1234abcd'
+        last_response.status.must_equal 200
+      end
+    end
+
+    describe "PUT /activities/:id" do
+      it "should return 404 if the activity is not found" do
+        Activity.stubs(:find_by_id).returns(nil)
+
+        put '/activities/1234abcd'
+        last_response.status.must_equal 404
+      end
+
+      it "should return 200 if the person is found" do
+        Activity.stubs(:find_by_id).returns("something")
+
+        put '/activities/1234abcd'
+        last_response.status.must_equal 200
+      end
+    end
+  end
+end
