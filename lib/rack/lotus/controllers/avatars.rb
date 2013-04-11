69fde5f0703dc4f4a9784f54086d241b9a7a56cc
wilkie <wilkie05@gmail.com>
2013-04-11 19:55:31 -0400

    Adds Avatars controller tests.

--- lib/rack/lotus/controllers/avatars.rb
+++ lib/rack/lotus/controllers/avatars.rb
@@ -5,10 +5,14 @@ module Rack
       avatar = Avatar.find_by_id(params[:id])
       status 404 and return unless avatar
 
-      size = params["size"].split('x')
+      size = params["size"].split('x').map(&:to_i)
+      status 404 and return if size.length != 2
 
       content_type avatar.content_type
-      avatar.read(size)
+      ret = avatar.read(size)
+
+      status 404 and return unless ret
+      ret
     end
   end
 end
---
+++ spec/controllers/avatars_spec.rb
@@ -0,0 +1,82 @@
+require_relative 'helper'
+require_controller 'avatars'
+
+class  Avatar; end
+module Lotus;  end
+
+describe Rack::Lotus do
+  before do
+    # Do not render
+    Rack::Lotus.any_instance.stubs(:render).returns("html")
+  end
+
+  describe "Avatars Controller" do
+    describe "GET /avatars/:id/:size" do
+      it "should return 404 if the avatar does not exist" do
+        Avatar.stubs(:find_by_id).returns(nil)
+
+        get '/avatars/bogus_id/48x48'
+        last_response.status.must_equal 404
+      end
+
+      it "should return the content type stored with the avatar" do
+        avatar = stub('Avatar')
+        avatar.stubs(:content_type).returns("image/some_image_type")
+        avatar.stubs(:read)
+        Avatar.stubs(:find_by_id).returns(avatar)
+
+        get '/avatars/valid_id/48x48'
+        content_type.must_equal("image/some_image_type")
+      end
+
+      it "should query the avatar for the given size" do
+        avatar = stub('Avatar')
+        avatar.stubs(:content_type).returns("image/some_image_type")
+        avatar.expects(:read).with([48, 48])
+        Avatar.stubs(:find_by_id).returns(avatar)
+
+        get '/avatars/valid_id/48x48'
+      end
+
+      it "should read the avatar data" do
+        avatar = stub('Avatar')
+        avatar.stubs(:content_type).returns("image/some_image_type")
+        avatar.stubs(:read).returns("DATA")
+        Avatar.stubs(:find_by_id).returns(avatar)
+
+        get '/avatars/valid_id/48x48'
+        last_response.body.must_equal "DATA"
+      end
+
+      it "should return 404 if avatar size not available" do
+        avatar = stub('Avatar')
+        avatar.stubs(:content_type).returns("image/some_image_type")
+        avatar.stubs(:read).returns(nil)
+        Avatar.stubs(:find_by_id).returns(avatar)
+
+        get '/avatars/valid_id/1x4800'
+        last_response.status.must_equal 404
+      end
+
+      it "should return 200 when successful" do
+        avatar = stub('Avatar')
+        avatar.stubs(:content_type).returns("image/some_image_type")
+        avatar.stubs(:read).returns("DATA")
+        Avatar.stubs(:find_by_id).returns(avatar)
+
+        get '/avatars/valid_id/48x48'
+        last_response.status.must_equal 200
+      end
+
+      it "should return 404 when avatar size doesn't make sense" do
+        avatar = stub('Avatar')
+        avatar.stubs(:content_type).returns("image/some_image_type")
+        avatar.stubs(:read).returns("DATA")
+        Avatar.stubs(:find_by_id).returns(avatar)
+
+        get '/avatars/valid_id/bogus'
+        last_response.status.must_equal 404
+      end
+    end
+  end
+end
