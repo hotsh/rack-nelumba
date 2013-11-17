7e5c0278c20394656d95792a779ff750e7f89beb
wilkie <wilkie05@gmail.com>
2013-11-17 05:12:52 -0500

    Allows new note route to be accessible.

--- lib/rack/lotus/controllers/notes.rb
+++ lib/rack/lotus/controllers/notes.rb
@@ -1,18 +1,19 @@
 module Rack
   class Lotus
+    # Retrieve a form to create a new Lotus::Note
+    get '/notes/new' do
+      # Must be logged in
+      status 404 and return if !current_person
+
+      render :haml, :"activities/notes/new"
+    end
+
     # Retrieve a Lotus::Note
     get '/notes/:id' do
       note = ::Lotus::Note.find_by_id(params["id"])
       status 404 and return if note.nil?
 
       render :haml, :"activities/notes/show", :locals => {:note => note}
-    end
-
-    get '/notes/new' do
-      # Must be logged in
-      status 404 and return if !current_person
-
-      render :haml, :"activities/notes/new", :locals => {:note => note}
     end
   end
 end
