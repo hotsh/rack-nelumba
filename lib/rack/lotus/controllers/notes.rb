faef34531ef73623aa6d5803d59c07ac71a64336
wilkie <wilkie05@gmail.com>
2013-11-17 05:04:13 -0500

    Adds new note page.

--- lib/rack/lotus/controllers/notes.rb
+++ lib/rack/lotus/controllers/notes.rb
@@ -5,7 +5,14 @@ module Rack
       note = ::Lotus::Note.find_by_id(params["id"])
       status 404 and return if note.nil?
 
-      render :haml, :"activities/note", :locals => {:note => note}
+      render :haml, :"activities/notes/show", :locals => {:note => note}
+    end
+
+    get '/notes/new' do
+      # Must be logged in
+      status 404 and return if !current_person
+
+      render :haml, :"activities/notes/new", :locals => {:note => note}
     end
   end
 end
