b8141f484e98f4378b2e77ad4956933a1df9fb10
wilkie <wilkie05@gmail.com>
2014-08-24 05:23:56 -0400

    Adds individual activity render in json/xml.

--- lib/rack/lotus/controllers/activities.rb
+++ lib/rack/lotus/controllers/activities.rb
@@ -5,17 +5,20 @@ module Rack
       activity = ::Lotus::Activity.find_by_id(params[:id])
       status 404 and return if activity.nil?
 
-      if request.preferred_type('text/html')
+      format = request.preferred_type(['text/html', 'application/json', 'application/atom+xml', 'application/xml'])
+
+      case format
+      when 'application/json'
+        content_type 'application/json'
+        activity.to_json
+      when 'application/atom+xml',
+           'application/xml'
+        content_type 'application/atom+xml'
+        activity.to_atom
+      when 'text/html'
         render :haml, :"activities/show", :locals => {
           :activity => activity
         }
-      elsif request.preferred_type('application/json')
-        content_type 'application/json'
-        activity.to_json
-      elsif request.preferred_type('application/atom+xml') ||
-            request.preferred_type('application/xml')
-        content_type 'application/atom+xml'
-        activity.to_atom
       else
         status 406
       end
--- spec/activities_spec.rb
+++ spec/activities_spec.rb
@@ -16,11 +16,55 @@ describe Rack::Lotus do
         last_response.status.must_equal 404
       end
 
-      it "should return 200 if the person is found" do
-        Lotus::Activity.stubs(:find_by_id).returns("something")
+      it "should return 200 if the activity is found" do
+        Lotus::Activity.stubs(:find_by_id).returns(Lotus::Activity.new)
 
         get '/activities/1234abcd'
         last_response.status.must_equal 200
+      end
+
+      it "should render activities/show" do
+        Lotus::Activity.stubs(:find_by_id).returns(Lotus::Activity.new)
+        Rack::Lotus.any_instance.expects(:render).with(anything,
+                                                       :"activities/show",
+                                                       anything)
+
+        get '/activities/1234abcd'
+      end
+
+      it "should return json when json is prioritized in accept" do
+        Lotus::Activity.stubs(:find_by_id).returns(Lotus::Activity.new)
+
+        accept "application/json"
+        get '/activities/1234abcd'
+
+        content_type.must_match "application/json"
+      end
+
+      it "should return atom when xml is prioritized in accept" do
+        Lotus::Activity.stubs(:find_by_id).returns(Lotus::Activity.new)
+
+        accept "application/xml"
+        get '/activities/1234abcd'
+
+        content_type.must_match "application/atom+xml"
+      end
+
+      it "should return atom when atom is prioritized in accept" do
+        Lotus::Activity.stubs(:find_by_id).returns(Lotus::Activity.new)
+
+        accept "application/atom+xml"
+        get '/activities/1234abcd'
+
+        content_type.must_match "application/atom+xml"
+      end
+
+      it "should return text/html by default" do
+        Lotus::Activity.stubs(:find_by_id).returns(Lotus::Activity.new)
+
+        get '/activities/1234abcd'
+
+        content_type.must_match "text/html"
       end
     end
 
