f1ac4695dcffd6128de5c613f297941083629e02
wilkie <wilkie05@gmail.com>
2014-08-24 03:29:41 -0400

    Adds individual activity render.

--- lib/rack/lotus/controllers/activities.rb
+++ lib/rack/lotus/controllers/activities.rb
@@ -4,6 +4,21 @@ module Rack
     get '/activities/:id' do
       activity = ::Lotus::Activity.find_by_id(params[:id])
       status 404 and return if activity.nil?
+
+      if request.preferred_type('text/html')
+        render :haml, :"activities/show", :locals => {
+          :activity => activity
+        }
+      elsif request.preferred_type('application/json')
+        content_type 'application/json'
+        activity.to_json
+      elsif request.preferred_type('application/atom+xml') ||
+            request.preferred_type('application/xml')
+        content_type 'application/atom+xml'
+        activity.to_atom
+      else
+        status 406
+      end
     end
 
     # Update the given activity if you own it.
